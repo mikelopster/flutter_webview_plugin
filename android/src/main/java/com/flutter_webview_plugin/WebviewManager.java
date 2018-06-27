6defe96f29615b1107fcfa3254a2e049a1a5121b
Tanitphon Paniwan <tanit.pani@gmail.com>
2018-06-27 19:21:27 +0700

    add package webchrome client

--- android/src/main/java/com/flutter_webview_plugin/WebviewManager.java
+++ android/src/main/java/com/flutter_webview_plugin/WebviewManager.java
@@ -11,6 +11,7 @@ import android.webkit.CookieManager;
 import android.webkit.ValueCallback;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.webkit.WebChromeClient;
 import android.widget.FrameLayout;
 
 import io.flutter.plugin.common.MethodCall;
