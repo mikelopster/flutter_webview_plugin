bca4d7dc8f1246317790aec6e7108dd282b82f78
Tanitphon Paniwan <tanit.pani@gmail.com>
2018-06-27 20:37:27 +0700

    Add enable geolocation

--- android/src/main/java/com/flutter_webview_plugin/WebviewManager.java
+++ android/src/main/java/com/flutter_webview_plugin/WebviewManager.java
@@ -49,6 +49,7 @@ class WebviewManager {
             }
         });
 
+        webView.setGeolocationEnabled(true);
         webView.setWebViewClient(webViewClient);
         webView.setWebChromeClient(new WebChromeClient() {
             public void onGeolocationPermissionsShowPrompt(String origin, GeolocationPermissions.Callback callback) {
